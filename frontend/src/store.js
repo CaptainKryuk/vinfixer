16f531c44c4edc88c720306064529c6941ec72bf
Pherss <andrey.kryukov@protonmail.com>
2022-02-17 13:20:19 +0300

    change frontend server

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import { createStore } from 'vuex'
 export default createStore({
   state() {
     return {
-      server: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/' : '',
+      server: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/' : 'https://api.vinfixer.com/api/',
       auth_headers: { 'Authorization': 'JWT ' + localStorage.getItem('access_token') },
     }
   },
